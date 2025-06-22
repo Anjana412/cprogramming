341ea8a6758837b0ed56b32b7b3ac735580c2d95
Anjana T <anjanat0001@gmail.com>
2025-06-22 13:20:30 +0530

    Add files via upload

---
+++ Hw/pttrn20.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,s;
+   for(i=1;i<=5;i++)
+   {
+       for(s=1;s<=5-i;s++)
+       {
+           printf(" ");
+       }
+       for(j=1;j<=(2*i-1);j++)
+       {
+           if(j==1||j==(2*i-1)||i==5)
+           {
+               printf("*");
+           }
+           else
+           {
+               printf(" ");
+           }
+       }   
+        printf("\n");
+       
+   }
+}
